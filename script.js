e674942c49d73716126ba56539b7c1b19f35ce91
Diego-Treuk <cacoloko07@gmail.com>
2026-05-08 23:25:21 -0300

    Implement step functions to change document title
    Added functions to update document title based on steps.

--- script.js
+++ script.js
@@ -10,3 +10,23 @@ avanca.forEach(button => {
         document.getElementById(proximoPasso).classList.add('ativo');
     })
 })
+
+function proximoPasso0() {
+    document.title = "Rota Longa";
+}
+
+function proximoPasso1() {
+    document.title = "Rota Curta";
+}
+
+function proximoPasso2() {
+    document.title = "Caminho sem volta";
+}
+
+function proximoPasso3() {
+    document.title = "Maldição";
+}
+
+function proximoPasso4() {
+    document.title = "A Verdade";
+}
